546526983a9c6fbcdf0dca6fa05ac50c36f4d6e5
Karthick-Office <karthick210701@outlook.com>
2023-10-19 19:16:40 +0530

    Update

---
+++ BDD Cucumber & Gherkin Assisted Practice Project/LearnCucumber/src/test/java/com/cucumber/definition/HooksTest.java
@@ -0,0 +1,34 @@
+package com.cucumber.definition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class HooksTest {
+	@Before(order = 0,value="@regression")
+	public void setup()
+	{
+		System.out.println("Executing only regression scenarios");
+		System.out.println("The feature sceanrio has started");
+		System.out.println("The Author is Karthick");
+		
+	}
+	@Before(order = 1,value="@db")
+	public void Dbsetup()
+	{
+		System.out.println("Connect to DB");
+	}
+	
+    @After(order = 1,value="@db")
+	public void DbClose()
+	{
+		System.out.println("Close the DB connection");
+	}
+
+	@After(order = 0,value="@regression")
+	public void teardown()
+	{
+		
+		System.out.println("The feature sceanrio has completed");
+		
+	}
+}
